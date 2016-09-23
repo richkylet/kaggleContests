17ca9d183d518bfb8a7c26ea5d9c7f4efd348498
Swetha Subramanian <swethasubramanian@gmail.com>
2016-09-23 13:10:11 -0700

    fixed a couple of indentation errors

--- HousePrices/MLScript.py
+++ HousePrices/MLScript.py
@@ -12,7 +12,7 @@ from sklearn import preprocessing
 from sklearn.metrics import mean_squared_error
 from sklearn import ensemble
 
-
+pd.options.mode.chained_assignment = None 
 
 
 def impute(Xi):
@@ -35,39 +35,39 @@ def impute(Xi):
 	Xi['YrSold'] = (Xi['YrSold'] - Xi['YrSold'].max())*-1.0
 	return Xi
 
-def encodeCols(Xi, cols):
+def encodeCols(Xi, cols1):
 	Qs = {'Ex':5, 'Gd': 4, 'TA':3, 'Fa':2, 'Po':1, 'NA':-1}
-	for col in cols:
-    	Xi[col] = Xi[col].apply(lambda x: Qs[x])
-    return Xi
+	for col in cols1:
+		Xi[col] = Xi[col].apply(lambda x: Qs[x])
+	return Xi
 
-def convCat2Num(Xi, cols):
-	for col in cols_that_need_to_be_encoded:
-    	Xi = pd.concat([Xi, pd.get_dummies(Xi[col]).rename(columns = lambda x:str(col)+'_'+x)], axis = 1)
-    for col in cols_that_need_to_be_encoded:
-    	del Xi[col]
-    return Xi
+def convCat2Num(Xi, cols2):
+	for col in cols2:
+		Xi = pd.concat([Xi, pd.get_dummies(Xi[col]).rename(columns = lambda x:str(col)+'_'+x)], axis = 1)
+	for col in cols2:
+		del Xi[col]
+	return Xi
 
 
-def preprocessing(Xi):
+def preprocessing_code(Xi):
 	Xi = impute(Xi)
 
 	# Encode ranked data
-	encodeCols = ['ExterQual', 'ExterCond', 'BsmtQual', 'BsmtCond', 'HeatingQC', 'KitchenQual',\
-        'FireplaceQu', 'GarageQual', 'GarageCond', 'PoolQC']
-    Xi = encodeCols(Xi)
+	ranked_cols = ['ExterQual', 'ExterCond', 'BsmtQual', 'BsmtCond', 'HeatingQC', 'KitchenQual',\
+	'FireplaceQu', 'GarageQual', 'GarageCond', 'PoolQC']
+	Xi = encodeCols(Xi, ranked_cols)
 
-    # One-hot-encode non categorical data
-    cols_that_need_to_be_encoded = Xi.select_dtypes(include = ['object']).columns
-    Xi = convCat2Num(Xi)
+	# One-hot-encode non categorical data
+	cols_that_need_to_be_encoded = Xi.select_dtypes(include = ['object']).columns
+	Xi = convCat2Num(Xi, cols_that_need_to_be_encoded)
 
-    # Drop shit that fell through the cracks
-    Xi = Xi.dropna()
+	# Drop shit that fell through the cracks
+	Xi = Xi.dropna()
 
-    # Min-Max Scaling
-    minmax = preprocessing.MinMaxScaler()
-    Xi.iloc[:,1:] = minmax.fit_transform(Xi.iloc[:,1:])
-    return Xi
+	# Min-Max Scaling
+	minmax = preprocessing.MinMaxScaler()
+	Xi.iloc[:,1:] = minmax.fit_transform(Xi.iloc[:,1:])
+	return Xi
 
 def rmse(y, y_pred):
 	mse_grad = mean_squared_error(y, y_pred)
@@ -76,7 +76,7 @@ def rmse(y, y_pred):
 def main():
 	# Load in the dataset
 	trainDf = pd.read_csv('train.csv')
-	testDf = pd.read_csv('test_csv')
+	testDf = pd.read_csv('test.csv')
 
 	# Split data sets
 	X = trainDf.iloc[:,:-1]
@@ -84,22 +84,22 @@ def main():
 	X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
 
 	# Important Features (ipython notebook) have been determined as:
-	not_zero = ['OverallQual', 'GrLivArea', '1stFlrSF', 'GarageCars', 'BsmtFinSF1',
-       'BsmtQual', 'KitchenQual', 'TotRmsAbvGrd', 'FireplaceQu',
-       'YearBuilt', 'ExterQual', 'LotArea', 'WoodDeckSF', 'MasVnrArea',
-       'GarageFinish_Unf']
-    
-    X_train = preprocessing(X_train)
+	not_zero = ['OverallQual', 'GrLivArea', '1stFlrSF', 'GarageCars', 'BsmtFinSF1',\
+	'BsmtQual', 'KitchenQual', 'TotRmsAbvGrd', 'FireplaceQu',\
+	'YearBuilt', 'ExterQual', 'LotArea', 'WoodDeckSF', 'MasVnrArea',\
+	'GarageFinish_Unf']
+
+	X_train = preprocessing_code(X_train)
 	y_train = y_train[X_train.index]
 
-	clf_grad = ensemble.GradientBoostingRegressor(random_state = 42, max_depth = 4)
+	clf_grad = ensemble.GradientBoostingRegressor(random_state = 42, max_depth = 4, learning_rate = 0.2)
 	grad_tree = clf_grad.fit(X_train[not_zero], y_train)
 
 	predicted_grad_train = grad_tree.predict(X_train[not_zero])
 	print rmse(y_train, predicted_grad_train)
 
 	## test
-	X_test = preprocessing(X_test)
+	X_test = preprocessing_code(X_test)
 	y_test = y_test[X_test.index]
 	predicted_grad_test = grad_tree.predict(X_test[not_zero])
 	print rmse(y_test, predicted_grad_test)
